5e97a7709d45764e9581ede42e4150bed2b45130
wes-mitchell <wmdrums@gmail.com>
2022-06-20 13:40:29 -0500

    initial commit

---
+++ DogGo/Controllers/OwnerController.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DogGo.Models;
+using DogGo.Repositories;
+using System.Collections.Generic;
+
+namespace DogGo.Controllers
+{
+    public class OwnerController : Controller
+    {
+        private readonly IOwnerRespository _ownerRepo;
+
+        public OwnerController(IOwnerRespository ownerRepo)
+        {
+            _ownerRepo = ownerRepo;
+        }
+
+
+        // GET: OwnerController
+        public ActionResult Index()
+        {
+            List<Owner> owners = _ownerRepo.GetAllOwners();
+
+            return View(owners);
+        }
+
+        // GET: OwnerController/Details/5
+        public ActionResult Details(int id)
+        {
+            Owner owner = _ownerRepo.GetOwnerById(id);
+
+            return View(owner);
+        }
+
+        // GET: OwnerController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: OwnerController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OwnerController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: OwnerController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: OwnerController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: OwnerController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ DogGo/Repositories/IOwnerRepository.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using Microsoft.Data.SqlClient;
+using DogGo.Models;
+
+namespace DogGo.Repositories
+{
+    public interface IOwnerRespository
+    {
+        List<Owner> GetAllOwners();
+        Owner GetOwnerById(int id);
+    }
+}
