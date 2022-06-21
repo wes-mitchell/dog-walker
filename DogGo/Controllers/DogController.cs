6f7009453ec82e43e6c6e0dc35c0ce850f1ba3a3
wes-mitchell <wmdrums@gmail.com>
2022-06-21 16:08:55 -0500

    completes delete dog method

--- DogGo/Controllers/DogController.cs
+++ DogGo/Controllers/DogController.cs
@@ -84,21 +84,25 @@ namespace DogGo.Controllers
         // GET: DogController/Delete/5
         public ActionResult Delete(int id)
         {
-            return View();
+            Dog dog = _dogRespository.GetById(id);
+
+            return View(dog);
         }
 
         // POST: DogController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public ActionResult Delete(int id, Dog dog)
         {
             try
             {
-                return RedirectToAction(nameof(Index));
+                _dogRespository.DeleteDog(id);
+
+                return RedirectToAction("Index");
             }
-            catch
+            catch (Exception)
             {
-                return View();
+                return View(dog);
             }
         }
     }
--- DogGo/Repositories/DogRepository.cs
+++ DogGo/Repositories/DogRepository.cs
@@ -146,7 +146,20 @@ namespace DogGo.Repositories
         }
         public void DeleteDog(int dogId)
         {
-            
+            using (SqlConnection conn = Connection)
+            {
+                conn.Open();
+                {
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = @"DELETE FROM Dog WHERE Id = @id";
+
+                        cmd.Parameters.AddWithValue("@id", dogId);
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+            }
         }
 
         //public List<Dog> GetDogsByOwnerID(int id)
